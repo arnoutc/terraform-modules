df1bb8adf187438a7983c38971d8d9fce76a6fd2
Arnout Cator <arnout.cator@gmail.com>
2023-01-31 19:47:06 +0000

    Initial commit of modules repo

---
+++ services/web-server-cluster/variables.tf
@@ -0,0 +1,54 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# REQUIRED PARAMETERS
+# You must provide a value for each of these parameters.
+# ---------------------------------------------------------------------------------------------------------------------
+
+variable "db_remote_state_bucket" {
+  description   = "The name of the S3 bucket used for the database's remote state storage"
+  type          = string
+}
+
+variable "db_remote_state_key" {
+  description   = "The name of the key in the S3 bucket used for the database's remote state storage"
+  type          = string
+}
+
+variable "instance_type" {
+  description = "The type of EC2 Instances to run (e.g. t2.micro)"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "min_size" {
+  description = "The minimum number of EC2 Instances in the ASG"
+  type        = number
+  default     = 2
+}
+
+variable "max_size" {
+  description = "The maximum number of EC2 Instances in the ASG"
+  type        = number
+  default     = 10
+}
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+# OPTIONAL PARAMETERS
+# These parameters have reasonable defaults.
+# ---------------------------------------------------------------------------------------------------------------------
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 80
+}
+
+variable "cluster_name" {
+  description = "The name to use to namespace all the resources in the cluster"
+  type        = string
+}
+
+variable "region" {
+    description = "Region used for state file"
+    type = string
+}
