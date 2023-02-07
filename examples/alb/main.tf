06d3e33deb0be7dcfb6e92616a50d54e3bd35114
Arnout Cator <arnout.cator@gmail.com>
2023-02-07 11:30:11 +0000

    updated alb example to a working condition

--- examples/alb/main.tf
+++ examples/alb/main.tf
@@ -3,7 +3,19 @@ provider "aws" {
 }
 
 module "alb" {
-  source="../../networking/alb"
+  source            ="../../networking/alb"
 
-  name = var.alb_name
+  alb_name          = var.alb_name
+  subnet_ids        = data.aws_subnets.default.ids
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
 }
