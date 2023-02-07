633054035797f835407b522c46647d2f5bf77549
Arnout Cator <arnout.cator@gmail.com>
2023-02-07 17:43:39 +0000

    added lambda and api gateway example

--- examples/lambda/main.tf
+++ examples/lambda/main.tf
@@ -20,7 +20,7 @@ resource "aws_lambda_function" "example" {
 
   # The bucket name as created earlier with "aws s3api create-bucket"
   s3_bucket = "pacator-terraform-serverless-example"
-  s3_key = "v1.0.0/mylambda.zip"
+  s3_key = "v${var.app_version}/mylambda.zip"
 
   # "main" is the filename within the zip file (main.js) and "handler"
   # is the name of the property under which the handler function was 
@@ -64,4 +64,16 @@ resource "aws_api_gateway_method" "proxy" {
   resource_id = "${aws_api_gateway_resource.proxy.id}"
   http_method = "ANY"
   authorization = "NONE"
+}
+
+resource "aws_lambda_permission" "apigw" {
+  statement_id = "AllowAPIGatewayInvoke"
+  action = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.example.function_name}"
+  principal = "apigateway.amazonaws.com"
+
+  # The /*/* portion grants access from any method on any resource
+  # within the API Gateway "REST API".
+  source_arn = "${aws_api_gateway_rest_api.example.execution_arn}/*/*"
+  
 }
