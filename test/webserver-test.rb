087f395287136292c1087323aece51cd9bafcc8f
Arnout Cator <arnout.cator@gmail.com>
2023-02-13 11:32:15 +0000

    added web-service ruby unit test and alb terratest

--- test/webserver-test.rb
+++ test/webserver-test.rb
@@ -2,11 +2,14 @@
 
 require 'test/unit'
 require './webserver'
+require 'net/http'
 
 class TestWebServer < Test::Unit::TestCase
+
     def initialize(test_method_name)
         super(test_method_name)
-        @handlers = Handlers.new
+        mock_web_service = MockWebService.new([200, 'text/html', 'mock example.org'])
+        @handlers = Handlers.new(mock_web_service)
     end
 
     def test_unit_hello
@@ -23,10 +26,35 @@ class TestWebServer < Test::Unit::TestCase
         assert_equal('{"foo":"bar"}', body)
     end
 
+    def test_unit_web_service
+        expected_status = 200
+        expected_content_type = 'text/html'
+        expected_body = 'mock example.org'
+        mock_response = [expected_status, expected_content_type, expected_body]
+
+        mock_web_service = MockWebService.new(mock_response)
+        handlers = Handlers.new(mock_web_service)
+        
+        status_code, content_type, body = handlers.handle("/web-service")
+        assert_equal(expected_status, status_code)
+        assert_equal(expected_content_type, content_type)
+        assert_equal(expected_body, body)
+    end
+
     def test_unit_404
         status_code, content_type, body = @handlers.handle("/invalid-path")
         assert_equal(404, status_code)
         assert_equal('text/plain', content_type)
         assert_equal('Not Found', body)
+    end
+end
+
+class MockWebService
+    def initialize(response)
+        @response = response
+    end
+
+    def proxy
+        @response
     end
 end
--- test/webserver.rb
+++ test/webserver.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 
+require 'net/http'
 require "webrick"
 
 =begin
@@ -18,7 +19,9 @@ require "webrick"
 
 class WebServer < WEBrick::HTTPServlet::AbstractServlet
     def do_GET(request, response)
-        handlers = Handlers.new
+        web_service = WebService.new("http://www.example.org")
+        handlers = Handlers.new(web_service)
+
         status_code, content_type, body = handlers.handle(request.path)
 
         response.status = status_code
@@ -27,13 +30,31 @@ class WebServer < WEBrick::HTTPServlet::AbstractServlet
     end
 end
 
+class WebService
+    def initialize(url)
+        @uri = URI(url)
+    end
+
+    def proxy
+        response = Net::HTTP.get_response(@uri)
+        [response.code.to_i, response['Content-Type'], response.body]
+    end
+end
+
 class Handlers
+    def initialize(web_service)
+        @web_service = web_service
+    end
+
     def handle(path)
         case path
         when "/"
             [200, 'text/plain', 'Hello, World']
         when "/api"
             [201, 'application/json', '{"foo":"bar"}']
+        when "/web-service"
+            # New endpoint that calls a web service
+            @web_service.proxy
         else
             [404, 'text/plain', 'Not Found']
         end
@@ -46,8 +67,10 @@ if __FILE__ == $0
     # Run the server on localhost at port 8000
     server = WEBrick::HTTPServer.new :Port => 8000
     server.mount '/', WebServer
+    
     # Shut down the server on CTRL+C
     trap 'INT' do server.shutdown end
+
     # Start the server
     server.start
 end
